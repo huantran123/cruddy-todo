c0735f7dfe6725830c7d16a95ba26b60ca5d3c63
Huan Tran <huanductran98@gmail.com>
2022-08-02 19:10:36 -0700

    Complete creating new todos

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,24 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  var id = '';
+  counter.getNextUniqueId((err, data) => {
+    if (err) {
+      throw error;
+    } else {
+      id += data;
+      var todoPath = path.join(exports.dataDir, `${id}.txt`);
+      fs.writeFile(todoPath, text, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          items[id] = text;
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
+
 };
 
 exports.readAll = (callback) => {
